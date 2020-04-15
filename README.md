38bc675fb9a5a8922679eeab46da0025693007fa
David Rolle <miridius@miridi.us>
2020-04-15 05:59:57 +0200

    Create verification function for signed reports
    Serde is used to seralize/deserialize the reports as JSON so that they
    can be passed between js and rust
    
    But to make Serde work a couple changes had to be made:
    - downgrade from Neon CLI 0.4 to 0.3
    - fork the TCN repo and add the Serialize/Deserialize traits

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Node.js Module that wraps the [TCN Protocol Rust Implementation](https://github.
 - [x] Automatically build binaries for various platforms so that we can run on GCP
 - [ ] Add tests
 - [ ] Expose full TCN API
+- [ ] Conversions from Strings etc. to/from byte arrays for convenience
+- [ ] Serialize TCN Errors
 - [ ] Use Typescript
 - [ ] Use Prettier & ESLint
 - [ ] Automatically compile/run on changes (nodemon + cargo-watch)
@@ -23,7 +25,50 @@ $ npm install covid19risk/tcn-node
 
 ```js
 import tcn from "tcn-node";
-console.log(tcn.tcn_example()); // should print "symptom data"
+
+console.log(tcn.tcn_example()); // => "symptom data"
+
+console.log(tcn.signed_report_example()) // => generates a signed report as JSON
+
+console.log(tcn.validate_report({
+  report: {
+    rvk: [[
+      205, 234, 147, 231, 210, 96, 99,
+      128, 241, 255, 168, 61, 243, 222,
+      144, 41, 194, 92, 112, 118, 140,
+      98, 90, 38, 156, 32, 216, 117,
+      171, 14, 206, 117
+    ]],
+    tck_bytes: [
+      5, 44, 47, 43, 14, 249, 162, 165,
+      139, 157, 225, 217, 38, 77, 151, 140,
+      247, 198, 138, 23, 208, 188, 229, 189,
+      20, 101, 126, 83, 216, 18, 194, 19
+    ],
+    j_1: 20,
+    j_2: 90,
+    memo_type: 'CoEpiV1',
+    memo_data: [
+      115, 121, 109, 112,
+      116, 111, 109, 32,
+      100, 97, 116, 97
+    ]
+  },
+  sig: {
+    R_bytes: [
+      171, 0, 174, 55, 138, 201, 100, 209,
+      69, 98, 176, 85, 27, 240, 129, 22,
+      204, 209, 89, 245, 9, 31, 170, 4,
+      1, 69, 243, 251, 36, 31, 249, 192
+    ],
+    s_bytes: [
+      250, 99, 139, 105, 167, 126, 136, 208,
+      253, 158, 225, 46, 81, 179, 50, 90,
+      113, 63, 235, 163, 172, 193, 251, 86,
+      76, 118, 188, 170, 16, 252, 132, 8
+    ]
+  }
+})) // => true
 ```
 
 ## Development
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,48 +1,71 @@
+#[macro_use]
+extern crate neon_serde;
+
 use neon::prelude::*;
 use std::str;
 use tcn::*;
 
-fn tcn_example(mut cx: FunctionContext) -> JsResult<JsString> {
-    // Generate a report authorization key.  This key represents the capability
-    // to publish a report about a collection of derived temporary contact numbers.
-    let rak = ReportAuthorizationKey::new(rand::thread_rng());
+export! {
 
-    // Use the temporary contact key ratchet mechanism to compute a list
-    // of temporary contact numbers.
-    let mut tck = rak.initial_temporary_contact_key(); // tck <- tck_1
-    let mut tcns = Vec::new();
-    for _ in 0..100 {
-        tcns.push(tck.temporary_contact_number());
-        tck = tck.ratchet().unwrap();
+    fn tcn_example() -> String {
+        // Generate a report authorization key.  This key represents the capability
+        // to publish a report about a collection of derived temporary contact numbers.
+        let rak = ReportAuthorizationKey::new(rand::thread_rng());
+
+        // Use the temporary contact key ratchet mechanism to compute a list
+        // of temporary contact numbers.
+        let mut tck = rak.initial_temporary_contact_key(); // tck <- tck_1
+        let mut tcns = Vec::new();
+        for _ in 0..100 {
+            tcns.push(tck.temporary_contact_number());
+            tck = tck.ratchet().unwrap();
+        }
+
+        // Prepare a report about a subset of the temporary contact numbers.
+        let signed_report = rak
+            .create_report(
+                MemoType::CoEpiV1,        // The memo type
+                b"symptom data".to_vec(), // The memo data
+                20,                       // Index of the first TCN to disclose
+                90,                       // Index of the last TCN to check
+            )
+            .expect("Report creation can only fail if the memo data is too long");
+
+        // Verify the source integrity of the report...
+        let report = signed_report
+            .verify()
+            .expect("Valid reports should verify correctly");
+
+        // ...allowing the disclosed TCNs to be recomputed.
+        let recomputed_tcns = report.temporary_contact_numbers().collect::<Vec<_>>();
+
+        // Check that the recomputed TCNs match the originals.
+        // The slice is offset by 1 because tcn_0 is not included.
+        assert_eq!(&recomputed_tcns[..], &tcns[20 - 1..90 - 1]);
+
+        // Read the memo data from the report
+        let memo_data =
+            str::from_utf8(report.memo_data()).expect("Could not convert memo bytes to string");
+
+        String::from(memo_data)
     }
 
-    // Prepare a report about a subset of the temporary contact numbers.
-    let signed_report = rak
-        .create_report(
-            MemoType::CoEpiV1,        // The memo type
-            b"symptom data".to_vec(), // The memo data
-            20,                       // Index of the first TCN to disclose
-            90,                       // Index of the last TCN to check
-        )
-        .expect("Report creation can only fail if the memo data is too long");
+    fn signed_report_example() -> SignedReport {
+        let rak = ReportAuthorizationKey::new(rand::thread_rng());
 
-    // Verify the source integrity of the report...
-    let report = signed_report
-        .verify()
-        .expect("Valid reports should verify correctly");
+        let signed_report = rak
+            .create_report(
+                MemoType::CoEpiV1,        // The memo type
+                b"symptom data".to_vec(), // The memo data
+                20,                       // Index of the first TCN to disclose
+                90,                       // Index of the last TCN to check
+            )
+            .expect("Report creation can only fail if the memo data is too long");
 
-    // ...allowing the disclosed TCNs to be recomputed.
-    let recomputed_tcns = report.temporary_contact_numbers().collect::<Vec<_>>();
+        signed_report
+    }
 
-    // Check that the recomputed TCNs match the originals.
-    // The slice is offset by 1 because tcn_0 is not included.
-    assert_eq!(&recomputed_tcns[..], &tcns[20 - 1..90 - 1]);
-
-    // Read the memo data from the report
-    let memo_data =
-        str::from_utf8(report.memo_data()).expect("Could not convert memo bytes to string");
-
-    Ok(cx.string(memo_data))
+    fn validate_report(signed_report: SignedReport) -> bool {
+        signed_report.verify().is_ok()
+    }
 }
-
-register_module!(mut cx, { cx.export_function("tcn_example", tcn_example) });
